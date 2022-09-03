49883e9edc081160c2ca98678320e9741879e0d9
sanchit <sanchit2ooosp1@gmail.com>
2022-09-03 19:35:30 +0530

    login router

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -83,7 +83,16 @@ router.post("/register", async (req, res) => {
     const emailVerificationToken = generateToken({id: user._id.toString()}, "30m")
     const url = `${process.env.BASE_URL}/api/users/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url)    
-    res.json(user); 
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: generateToken({id: user._id.toString()}, "7d"),
+      verified: user.verified,
+      message: "Register success! Please activate your email to start"
+    }); 
   } catch (error) {
     res.status(500).json({ message: error.message });
   } 
@@ -98,6 +107,33 @@ router.post('/activate', async(req, res)=> {
     await User.findByIdAndUpdate(user.id, {verified: true});
     return res.status(200).json({message: "Account has been activated successfully"})
   }
+})
+router.post('/login', async(req, res)=> {
+  try{
+    const {email, password} = req.body;
+    const user = await User.findOne({email: email});
+    if(!user){
+      return res.status(400).json({message: "this email address you entered is not found"}) 
+    }
+    if(bcrypt.compareSync(password, user.password)) {
+        return res.send({
+          id: user._id,
+          username: user.username,
+          picture: user.picture,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          token: generateToken({id: user._id.toString()}, "7d"),
+          verified: user.verified,
+          message: "Register success! Please activate your email to start"
+        });
+    }else {
+      return res.status(400).json({message: "Invalid credentials. Please try again."})
+    }
+    
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  } 
 })
 
 export default router;
